05a5631eb55ddc3b4c6810e5792d68740909f234
AtayErgun <atayergun29@gmail.com>
2023-05-26 20:03:36 +0300

    Firebase de Realtime Database kısmı
    Firebase Database ortamı kuruldu,
    Database e manuel olarak veriler yüklendi

--- KafeSiparisProgrami/MainWindow.Designer.cs
+++ KafeSiparisProgrami/MainWindow.Designer.cs
@@ -29,13 +29,24 @@ namespace KafeSiparisProgrami
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 106);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(808, 450);
+            this.Controls.Add(this.button1);
             this.Name = "MainWindow";
             this.Text = "Sipariş Programı";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
@@ -44,5 +55,7 @@ namespace KafeSiparisProgrami
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- KafeSiparisProgrami/MainWindow.cs
+++ KafeSiparisProgrami/MainWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Firebase.Auth;
+using Firebase.Database;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,11 +15,27 @@ namespace KafeSiparisProgrami
     public partial class MainWindow : Form
     {
         private UserCredential kullanici_kimligi;
+        private FirebaseClient firebaseistemci;
         public MainWindow(UserCredential kullanici_kimligi)
         {
             InitializeComponent();
             this.kullanici_kimligi = kullanici_kimligi;
             this.Text = this.Text + " |Kullanici: " + kullanici_kimligi.User.Info.Email;
+            try
+            { 
+            firebaseistemci = new FirebaseClient("https://kafesiparisprogrami-default-rtdb.firebaseio.com/",
+                new FirebaseOptions
+                {
+                    AuthTokenAsyncFactory = () => kullanici_kimligi.User.GetIdTokenAsync()
+                });
+                MessageBox.Show("Firebase Realtime Database için istemci oluşturuldu.." , "Başarılı!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Mesaj: " + exc.Message, "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
